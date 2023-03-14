1a988b8ae8ba0157e0848485a6d1c36081527004
혀느현스 <hyuns@hyuns.dev>
2023-03-14 10:12:04 +0900

    add day3, 4

--- day4/2.cpp
+++ day4/2.cpp
@@ -4,10 +4,12 @@
 using namespace std;
 
 int main() {
-    double f_temp = 70.0;
-    double c_temp;
+    cout << "화씨 온도는 몇 도 입니까? ";
+    
+    double f_temp;
+    cin >> f_temp;
 
-    c_temp = (5.0 / 9.0) * (f_temp - 32);
+    double c_temp = (5.0 / 9.0) * (f_temp - 32);
     cout << "화씨온도 " << f_temp << "도는 섭씨온도" << c_temp << "입니다." << endl;
 
     return 0;
---
+++ day4/3.cpp
@@ -0,0 +1,22 @@
+
+    
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "x의 갑은? ";
+    double x;
+    cin >> x;
+
+    cout << "x의 갑은? ";
+    double y;
+    cin >> y;
+
+    cout << "x + y = " << x + y << endl;
+    cout << "x - y = " << x - y << endl;
+    cout << "x * y = " << x * y << endl;
+    cout << "x / y = " << x / y << endl;
+
+    return 0;
+}
